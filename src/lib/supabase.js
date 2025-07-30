b61feb2f70ff7bba243cdeed014bb6e9a70ce1a1
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2025-07-30 18:53:18 +0200

    Create supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,11 @@
+// /src/lib/supabase.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
+
+if (!supabaseUrl || !supabaseServiceKey) {
+  throw new Error('Supabase environment variables are missing');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
