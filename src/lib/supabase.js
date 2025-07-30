445ea677f23ac21944fa84f7cfb48030ac090daf
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2025-07-30 23:41:47 +0200

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,11 +1,11 @@
-// /src/lib/supabase.js
+// src/lib/supabase.js
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
 
 if (!supabaseUrl || !supabaseServiceKey) {
-  throw new Error('Supabase environment variables are missing');
+  throw new Error('❌ Supabase environment variables are missing');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseServiceKey);
