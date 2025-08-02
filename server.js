d5288018a6131535b45d89e5773ea6576ab5d103
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2025-08-02 13:43:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
-app.use('/webhook', stripeWebHookRouter);
+app.use('/webhook', stripeWebHook);
 app.use(express.static(path.join(__dirname, 'public')));
 
 const supabase = createClient(
